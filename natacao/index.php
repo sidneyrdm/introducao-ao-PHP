eba2a31e3da67131a9534ffc7bc4e4cf1d26087b
sidneyrdm <sidneybritomal@gmail.com>
2021-07-27 13:47:15 -0300

    Adicionando validação dos campos do formulário

---
+++ natacao/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Inscrição</title>
+</head>
+<body>
+    <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+    <hr>
+    <form action = "script.php" method="post">
+    <p>Seu Nome.: <Input type="text" name="nome" /></p>    
+    <p>Sua Idade.: <Input type="text" name="idade" /></p>
+    <p><Input type="submit" value="Enviar dados do competidor"/></p>
+    </form>
+</body>
+</html>
+
+
+
--- natacao/script.php
+++ natacao/script.php
@@ -4,8 +4,28 @@ $categorias = ['infantil', 'adolescente', 'adulto'];
 
 //print_r($categorias);
 
-$nome = 'Jonatas';
-$idade = 19;
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+if(empty($nome)){
+    echo 'O nome não pode ser Vazio';
+    return;
+}
+
+if(strlen($nome) < 3){
+    echo 'O nome deve conter no mínimo 3 caracteres';
+    return;
+}
+
+if(strlen($nome) > 40){
+    echo 'O nome não poder ter mais do que 40 caracteres';
+    return;
+}
+
+if(!is_numeric($idade)){
+    echo 'Digite uma idade válida';
+    return;
+}
 
 if($idade >= 6 && $idade <= 12){
     echo 'Infantil';
@@ -21,9 +41,5 @@ echo 'O nadador '.$nome." pertence a categoria ".$categorias[1];
     echo '<br><hr>';
     echo 'O nadador '.$nome." pertence a categoria ".$categorias[2];
 }
-
-var_dump ($idade);
-
-//echo count($categorias);
 
 
